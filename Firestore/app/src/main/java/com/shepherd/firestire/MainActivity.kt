7a378f1a0072dc5df91522304da03dc4ca0cd81c
Chun-Chieh Liang <maple3312936@outlook.com>
2019-01-24 21:54:50 -0500

    firebase auth

--- Firestore/app/src/main/java/com/shepherd/firestire/MainActivity.kt
+++ Firestore/app/src/main/java/com/shepherd/firestire/MainActivity.kt
@@ -1,28 +1,84 @@
 package com.shepherd.firestire
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.firestore.FirebaseFirestore
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.shepherd.firestire.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    val mikeUID = "7Zqjyup8b7CKLyHi6usg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val eventsSnapshot = FirebaseFirestore.getInstance().collection("users").get()
 
-        binding.apply {
-            rvEventList.hasFixedSize()
-            rvEventList
-        }
+        binding.fabAdd.setOnClickListener { signOut() }
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            tv_user.text = auth.currentUser!!.displayName
+        } else {
+            themeAndLogo()
+        }
+    }
+
+    private fun themeAndLogo() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.FacebookBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.AnonymousBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.mipmap.ic_launcher_round)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                Snackbar.make(root, "Sign out successfully!", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
     companion object {
-        val TAG = MainActivity.toString()
+        private const val RC_SIGN_IN = 123
+        private val TAG = MainActivity.toString()
     }
 }
